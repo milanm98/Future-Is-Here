91561a807fbc2cac948799ab68b3f3f8ca72d5d3
milanm98 <marjanovmilan13@gmail.com>
2021-07-07 11:52:59 +0200

    landing page

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,8 +1,12 @@
+
 function navigation(){
     return (
         <main>
-            <div className="w-full h-16 bg-black text-white flex flex-row justify-center items-center">
-                <a href="#">Careers</a>
+            <div className="font-mono w-full h-16 bg-black text-2xl text-white flex flex-row items-center gap-24">
+                <button className="ml-20">WHO WE ARE?</button>
+                <button>PROJECTS</button>
+                <button>CONTACT</button>
+                <button>CAREERS</button>
             </div>
         </main>
     )
